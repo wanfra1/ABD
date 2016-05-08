ac23e62b4d89a1e2548096ac49495969897db853
Curro <mayantigu@gmail.com>
2016-05-08 23:51:09 +0200

    Adding correct charset.

--- Abd/comun.php
+++ Abd/comun.php
@@ -4,10 +4,11 @@ class DataBase {
     private $username = 'root';
     private $password = '';
     private $dbName = 'almacen_tienda_stock';
+    private $charset = 'utf8';
     public $dbh = NULL;
     public function __construct() {
         try  {
-            $this->dbh = new PDO("mysql:host=$this->hostname;dbname=$this->dbName", $this->username, $this->password);
+            $this->dbh = new PDO("mysql:host=$this->hostname;dbname=$this->dbName;charset=$this->charset", $this->username, $this->password);
         } catch(PDOException $e) {
             echo __LINE__.$e->getMessage();
         }
