7b8e45ff0ea7bd0a8f1751ce1a339f72ae364e34
wanfra1 <juanfranciscomera@gmail.com>
2016-05-09 23:32:59 +0200

    some changes :)

--- Abd/servicios/pedidos.php
+++ Abd/servicios/pedidos.php
@@ -5,4 +5,13 @@ class Pedidos {
         $bd = new BaseDatos();
         return $bd->getQuery('SELECT * FROM `stock_pedido`');
     }
+    public function guardar($almacen, $proveedor, $productosExistentes) {
+        $bd = new BaseDatos();
+        $query = 'INSERT INTO `stock_pedido` (ID, ID_ALMACEN, ID_PROVEEDOR) VALUES (NULL, '.$almacen.', "'.$proveedor.'")';
+        $result = $bd->insertarConId($query);
+        foreach ($productosExistentes as $i=>$producto) {
+            $bd->getQuery('INSERT INTO `stock_lineapedido` (ID_PEDIDO, ID_PRODUCTO, UNIDADES) VALUES ('.$result.', '.$producto['producto'].', '.$producto['cantidad'].')');
+        }
+        return $result;
+    }
 }
