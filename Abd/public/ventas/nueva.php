9f85fa5ec8bae5952dc2a86173169a27715f69bc
Curro <mayantigu@gmail.com>
2016-05-09 18:50:58 +0200

    Adding validations.

--- Abd/public/ventas/nueva.php
+++ Abd/public/ventas/nueva.php
@@ -10,30 +10,73 @@
 <?php include '../../servicios/tiendas.php';?>
 <?php include '../../servicios/almacenes.php';?>
 <?php include '../../servicios/productos.php';?>
+<?php
+    $descripcion = '';
+    $tienda = '';
+    $almacen = '';
+    $productos = array();
+    $errorDescripcion = '';
+    $errorTienda = '';
+    $errorAlmacen = '';
+    $errorProductos = array();
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (empty($_POST['descripcion'])) {
+            $errorDescripcion = 'Por favor introduzca una descripción';
+        } else {
+            $descripcion = $_POST['descripcion'];
+        }
+
+        if (empty($_POST['tienda'])) {
+            $errorTienda = 'Por favor seleccione una tienda destino';
+        } else {
+            $tienda = $_POST['tienda'];
+        }
+
+        if (empty($_POST['almacen'])) {
+            $errorAlmacen = 'Por favor seleccione un almacén de destino';
+        } else {
+            $almacen = $_POST['almacen'];
+        }
+    }
+?>
 <div id="divForm">
-    <form method="post" action="crearVenta.php" enctype="multipart/form-data">
+    <form method="post" action="nueva.php" enctype="multipart/form-data">
         <label id="label_descripcion" for="descripcion">Descripcion:</label>
-        <input type="text" name="descripcion" id="descripcion">
+        <input type="text" name="descripcion" id="descripcion" value="<?php echo $descripcion; ?>">
+        <?php echo $errorDescripcion; ?>
         <label id="label_tienda" for="tienda">Tienda:</label>
         <select id="tienda" name="tienda">
             <?php
                 $tiendas = new Tiendas();
                 $todos = $tiendas->todos();
+                echo '<option value="">Seleccione una opción</option>';
                 foreach ($todos as $row) {
-                    echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+                    if ($row[0] == $tienda) {
+                        echo '<option value="'.$row[0].'" selected>'.$row[1].'</option>';
+                    } else {
+                        echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+                    }
                 }
             ?>
         </select>
+        <?php echo $errorTienda; ?>
         <label id="label_almacen" for="almacen">Almacén:</label>
         <select id="almacen" name="almacen">
             <?php
             $almacenes = new Almacenes();
             $todos = $almacenes->todos();
+            echo '<option value="">Seleccione una opción</option>';
             foreach ($todos as $row) {
-                echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+                if ($row[0] == $almacen) {
+                    echo '<option value="'.$row[0].'" selected>'.$row[1].'</option>';
+                } else {
+                    echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+                }
             }
             ?>
         </select>
+        <?php echo $errorAlmacen; ?>
         <div id="divProductos" class="divProductos">
             <?php
                 $productos = new Productos();
