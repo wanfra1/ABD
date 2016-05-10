be5ca82e4f6f7f92371a177e7a29540984f45b9c
Curro <mayantigu@gmail.com>
2016-05-10 11:25:55 +0200

    estilos.

--- Abd/servicios/pedidos.php
+++ Abd/servicios/pedidos.php
@@ -7,10 +7,15 @@ class Pedidos {
     }
     public function guardar($almacen, $proveedor, $productosExistentes) {
         $bd = new BaseDatos();
-        $query = 'INSERT INTO `stock_pedido` (ID, ID_ALMACEN, ID_PROVEEDOR) VALUES (NULL, '.$almacen.', "'.$proveedor.'")';
+        $query = 'INSERT INTO `stock_pedido` (ID_ALMACEN, ID_PROVEEDOR) VALUES ('.$almacen.', "'.$proveedor.'")';
         $result = $bd->insertarConId($query);
         foreach ($productosExistentes as $i=>$producto) {
             $bd->getQuery('INSERT INTO `stock_lineapedido` (ID_PEDIDO, ID_PRODUCTO, UNIDADES) VALUES ('.$result.', '.$producto['producto'].', '.$producto['cantidad'].')');
+            $stockActual = $bd->getQuery('SELECT * FROM `stock_producto_almacen` WHERE ID_ALMACEN='.$almacen.' AND ID_PRODUCTO='.$producto['producto']);
+            foreach ($stockActual as $stock) {
+                $unidades = $stock[2] + $producto['cantidad'];
+                $bd->runQuery('UPDATE `stock_producto_almacen` SET unidades='.$unidades.' WHERE ID_ALMACEN='.$almacen.' AND ID_PRODUCTO='.$producto['producto']);
+            }
         }
         return $result;
     }
