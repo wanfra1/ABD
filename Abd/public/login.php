e9864f8491856b5a601c40653d8c769ff33a5f4a
Curro <mayantigu@gmail.com>
2016-05-29 17:00:59 +0200

    Securizando.

--- Abd/public/login.php
+++ Abd/public/login.php
@@ -1,6 +1,7 @@
 <?php
 include '../servicios/bd.php';
 include '../servicios/usuarios.php';
+$errorLogin = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['username']) &&
         $_POST['username'] != '' &&
@@ -20,49 +21,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['username'] = $_POST['username'];
             header('Location: ventas/lista.php');
         } else {
-            echo 'Credenciales incorrectos';
-            echo md5($_POST['password']);
+            $errorLogin = 'Credenciales incorrectos';
         }
     } else {
-        echo 'Debes rellenar un usuario y contraseña';
+        $errorLogin = 'Credenciales incorrectos';
     }
 }
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    </head>
-    <body>
-        <nav class="navbar navbar-inverse navbar-fixex-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Promotores Ana</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                </div><!--/.navbar-collapse -->
-            </div>
-        </nav>
-        <div class="container">
-            <form id="login" method="post" action="./login.php">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" name="username" class="form-control" id="exampleInputEmail1" placeholder="Introduzca su email">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Introduzca su password">
-                </div>
-                <button type="submit" class="btn btn-default">Login</button>
-            </form>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixex-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Promotores Ana</a>
         </div>
-    </body>
+        <div id="navbar" class="navbar-collapse collapse">
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
+<div class="container">
+    <?php if ($errorLogin != '') {
+        echo '<div class="alert alert-warning">'.$errorLogin.'</div>';
+    }?>
+    <form id="login" method="post" action="./login.php">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" name="username" class="form-control" id="exampleInputEmail1" placeholder="Introduzca su email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Introduzca su password">
+        </div>
+        <button type="submit" class="btn btn-default">Login</button>
+    </form>
+</div>
+</body>
 </html>
