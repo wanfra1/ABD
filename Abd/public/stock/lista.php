bccc639580c7278dcb7a3a664a698848d7a4f816
Curro <mayantigu@gmail.com>
2016-05-29 19:28:55 +0200

    Añadiendo logout.

--- Abd/public/stock/lista.php
+++ Abd/public/stock/lista.php
@@ -3,14 +3,36 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Consultar stock</title>
-    <link rel="stylesheet" type="text/css" href="../../static/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
 <?php include '../../servicios/bd.php';?>
 <?php include '../../servicios/stock.php';?>
 <?php include '../../servicios/almacenes.php';?>
 <?php include '../../servicios/productos.php';?>
-
+<div class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Promotores Ana</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="../ventas/lista.php">Ventas</a></li>
+                <li><a href="../stock/lista.php">Stock</a></li>
+                <li><a href="../pedidos/nuevo.php">Pedidos</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../logout.php">Salir</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 <div class="container">
     <h1 class="page-header col-md-12">Consultar stock</h1>
     <?php
